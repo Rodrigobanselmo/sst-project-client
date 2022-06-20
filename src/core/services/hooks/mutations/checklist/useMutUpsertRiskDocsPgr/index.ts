b17496dcbc605ec610699b01033a55ded65c08d3
Rodrigo Barbosa Anselmo <rodrigobanselmo@gmail.com>
2022-06-20 02:33:23 -0300

    feat: generate test

---
+++ src/core/services/hooks/mutations/checklist/useMutUpsertRiskDocsPgr/index.ts
@@ -0,0 +1,76 @@
+import { useMutation } from 'react-query';
+
+import { useSnackbar } from 'notistack';
+import { StatusEnum } from 'project/enum/status.enum';
+
+import { ApiRoutesEnum } from 'core/enums/api-routes.enums';
+import { QueryEnum } from 'core/enums/query.enums';
+import { useGetCompanyId } from 'core/hooks/useGetCompanyId';
+import { IPrgDocData } from 'core/interfaces/api/IRiskData';
+import { api } from 'core/services/apiClient';
+import { queryClient } from 'core/services/queryClient';
+import { downloadFile } from 'core/utils/helpers/downloadFile';
+
+import { IErrorResp } from '../../../../errors/types';
+
+export interface IUpsertRiskDocs {
+  id?: string;
+  name: string;
+  riskGroupId: string;
+  workspaceId: string;
+  workspaceName: string;
+  description?: string;
+  version?: string;
+  status?: StatusEnum;
+  companyId?: string;
+}
+
+export async function upsertRiskDocs(
+  data: IUpsertRiskDocs,
+  companyId?: string,
+) {
+  if (!companyId) return null;
+
+  const response = await api.post<IPrgDocData>(
+    `${ApiRoutesEnum.DOCUMENTS_PGR}/doc`,
+    {
+      companyId,
+      ...data,
+    },
+    {
+      responseType: 'blob',
+    },
+  );
+
+  downloadFile(response);
+
+  return data;
+}
+
+export function useMutUpsertRiskDocsPgr() {
+  const { getCompanyId } = useGetCompanyId();
+  const { enqueueSnackbar } = useSnackbar();
+
+  return useMutation(
+    async (data: IUpsertRiskDocs) => upsertRiskDocs(data, getCompanyId(data)),
+    {
+      onSuccess: async (resp) => {
+        if (resp)
+          queryClient.refetchQueries([
+            QueryEnum.RISK_GROUP_DOCS,
+            resp.companyId,
+            resp.riskGroupId,
+          ]);
+
+        enqueueSnackbar('Documento editado com sucesso', {
+          variant: 'success',
+        });
+        return resp;
+      },
+      onError: (error: IErrorResp) => {
+        if (error.response?.data)
+          enqueueSnackbar(error.response.data.message, { variant: 'error' });
+      },
+    },
+  );
+}
--- src/pages/dashboard/dados/index.page.tsx
+++ src/pages/dashboard/dados/index.page.tsx
@@ -2,12 +2,26 @@ import { SContainer } from 'components/atoms/SContainer';
 import { DatabaseTable } from 'components/organisms/tables/DatabaseTable';
 import { NextPage } from 'next';
 
+import { useMutUpsertRiskDocsPgr } from 'core/services/hooks/mutations/checklist/useMutUpsertRiskDocsPgr';
 import { withSSRAuth } from 'core/utils/auth/withSSRAuth';
 
 const Database: NextPage = () => {
+  const createDoc = useMutUpsertRiskDocsPgr();
+
+  const onGeneratePGRDocumentTest = () => {
+    return createDoc.mutateAsync({
+      version: Math.random().toString(),
+      name: 'doc_name',
+      riskGroupId: '7868bea7-3623-4d48-98c4-0c2c297a79fc',
+      workspaceId: '48707b4d-e011-44e2-a4fa-bded0d9efe41',
+      workspaceName: 'Matriz',
+    });
+  };
+
   return (
     <SContainer>
       <DatabaseTable />
+      <button onClick={onGeneratePGRDocumentTest}>documento PGR teste</button>
     </SContainer>
   );
 };
