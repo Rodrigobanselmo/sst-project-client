d2e05a375675c226e5383924aa37e37c15084452
Rodrigo Barbosa Anselmo <rodrigobanselmo@gmail.com>
2024-09-25 21:38:27 -0300

    fix: remove pagination

--- src/@v2/components/organisms/STable/addons/addons-table/STablePagination2/STablePagination.types.ts
+++
@@ -1,9 +0,0 @@
-import { BoxProps } from '@mui/material';
-
-/* eslint-disable @typescript-eslint/ban-types */
-export type STablePaginationProps = BoxProps & {
-  totalCountOfRegisters?: number;
-  registersPerPage?: number;
-  currentPage?: number;
-  onPageChange: (page: number) => void;
-};
--- src/@v2/components/organisms/STable/addons/addons-table/STablePagination2/components/PaginationItem/SPaginationItem.types.ts
+++
@@ -1,7 +0,0 @@
-import { BoxProps } from '@mui/material';
-
-export type PaginationItemProps = BoxProps & {
-  pageNumber: number;
-  isCurrent?: boolean;
-  onPageChange: (page: number) => void;
-};
