7dfe872b100d39f7422766dbf7cd6aafc65b9949
Alex Abreu Marins <alex@Alexs-MacBook-Pro.local>
2026-04-22 16:19:05 -0300

    fix: remove SSR from form application page to avoid Vercel timeout

--- src/pages/dashboard/empresas/[companyId]/formularios/aplicados/[id]/index.page.tsx
+++ src/pages/dashboard/empresas/[companyId]/formularios/aplicados/[id]/index.page.tsx
@@ -1,16 +1,9 @@
 import { NextPage } from 'next';
 
 import { FormViewPage } from '@v2/pages/companies/forms/pages/application/pages/view/form-application-view.page';
-import { withSSRAuth } from 'core/utils/auth/withSSRAuth';
 
 const Page: NextPage = () => {
   return <FormViewPage />;
 };
 
 export default Page;
-
-export const getServerSideProps = withSSRAuth(async () => {
-  return {
-    props: {},
-  };
-});
